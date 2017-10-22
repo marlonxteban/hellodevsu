a6ce074bbd4e3b3df6321bdd462de6b7d40357fe
marlon olaya <marlonxteban@gmail.com>
2017-10-21 22:20:44 -0500

    cambio de ip de servidor de api, en lugar de localhost toma la ipv4 del sistema

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from bottle import route, run
 from bottle import response
 from json import dumps
 
+#obtencion de la IP del sistema
+import socket
+ip4 = socket.gethostbyname_ex(socket.gethostname())
+ip4 = ip4[2][-1]
+
+#API hello {name}
 @route('/hello/:name')
 def index(name='World'):
-    resp = { "message":"Hello "+name }
+    resp = { "message":"Hello "+name}
     return dumps(resp)
 
-run(host='localhost', port=8080)
+#Iniciando el servidor en la ip del sistema
+run(host=ip4, port=8080)
